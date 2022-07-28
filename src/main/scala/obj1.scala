b6b34a8973408d1f402da8f15932a2ccd3c3a250
Palle Anusha Reddy <palleanushareddy95@gmail.com>
2022-07-28 13:22:47 +0530

    commit

---
+++ src/main/scala/obj1.scala
@@ -0,0 +1,11 @@
+object obj1 extends  App {
+println("Hi, This is Anusha")
+  val l=List(1,2,3,4,5,6)
+  println(l.map(x=>x.toFloat))
+  val a=Array("Anusha","Reddy","Palle")
+  println(a.mkString("|"))
+for(i<-a)
+  if (i=="Reddy")
+    {println("Data Engineer")}
+  else {println("DE")}
+}
