81e07b30acf4676a5b18c13dd18ca534a072d691
palleanusha95 <35245137+palleanusha95@users.noreply.github.com>
2022-07-28 11:56:11 +0530

    Update playGround.scala

--- src/main/scala/playGround/playGround.scala
+++ src/main/scala/playGround/playGround.scala
@@ -2,4 +2,9 @@ package playGround
 
 object playGround extends App{
 println("Hi There")
+ val gender='F'
+  if(gender=='F') then
+println("lets pay football")
+  else
+  println("lets play hockey")
 }
